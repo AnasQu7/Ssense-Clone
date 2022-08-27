325186f2b3bb1e53971feef16a44a5f8a2e25e57
naman kumar singh <singhnaman320@gmail.com>
2022-08-27 21:12:19 +0530

    last

---
+++ index.js
@@ -0,0 +1,11 @@
+//Importing navbar
+
+import nav from "./navbar.js";
+document.getElementById("navbar").innerHTML= nav();
+
+//****************************************************************************************
+
+import footer from "./footer.js";
+document.getElementById("footer").innerHTML= footer();
+
+//*****************************************************************************************
